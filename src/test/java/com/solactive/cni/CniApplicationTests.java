0e4c7b8dcd6407e14b18ea4ebf9d1d3550c176d1
debalaxmi-sahoo <sahoo.debalaxmi@gmail.com>
2021-06-08 11:42:29 +0530

    src folder

---
+++ src/test/java/com/solactive/cni/CniApplicationTests.java
@@ -0,0 +1,18 @@
+package com.solactive.cni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	public void testProcessBookings() throws Exception {
+		
+	}
+
+}
